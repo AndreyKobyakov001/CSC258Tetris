2e5e9120c9daa53ca31418149786a20553a2429d
Andrey Kobyakov <84627486+AndreyKobyakov001@users.noreply.github.com>
2024-04-03 17:06:41 -0400

    Added number splitter; use stack or similar to allow for all 4 to be printed.

--- numbers.asm
+++ numbers.asm
@@ -1,14 +1,60 @@
 #DRAWING FUNCTIONS - TODO: remove $t0 addr_dspl initialization and replace where the score is updated in game_loop to appropriately change values. Offset of 0, 20, 40, 60 for each digit of 4-digit score. 
-draw_zero: 
+.data
+##############################################################################
+# Immutable Data
+##############################################################################
+# The address of the bitmap display. Don't forget to connect it!
+ADDR_DSPL:
+    .word 0x10008000
+SCORE: .word 5234
+    
+.text
+
+score_selector:
+    lw $t3, SCORE
     lw $t0, ADDR_DSPL
-    # TO BE REMOVED; initialize $t0 at summoning point
-    #Each respective digit of ABCD takes an offset of 0, 20, 40, 60 - an exercise to the implementer. 
-    # addi $t0, $t0, 60
-    addi $t0, $t0, 1452 #TOP HORIZONTAL
+    addi $t0, $t0, 1452 
+    
+    div $a2, $t3, 1000 
+    jal select_draw_function
+    
+    mul $t4, $a2, 1000
+    sub $t3, $t3, $t4 
+    div $a2, $t3, 100
+    addi $t0, $t0, 20
+    jal select_draw_function
+    
+    mul $t4, $a2, 100
+    sub $t3, $t3, $t4 
+    div $a2, $t3, 10
+    addi $t0, $t0, 20
+    jal select_draw_function
+    
+    mul $t4, $a2, 10
+    sub $t3, $t3, $t4 
+    div $a2, $t3, 1
+    addi $t0, $t0, 20
+    jal select_draw_function
+    
+    
+    
+select_draw_function:
+    beq $a2, $zero, draw_zero    # If digit is 0, call draw_zero
+    beq $a2, 1, draw_one       # If digit is 1, call draw_one
+    beq $a2, 2, draw_two       # If digit is 2, call draw_two
+    beq $a2, 3, draw_three     # If digit is 3, call draw_three
+    beq $a2, 4, draw_four      # If digit is 4, call draw_four
+    beq $a2, 5, draw_five      # If digit is 5, call draw_five
+    beq $a2, 6, draw_six       # If digit is 6, call draw_six
+    beq $a2, 7, draw_seven     # If digit is 7, call draw_seven
+    beq $a2, 8, draw_eight     # If digit is 8, call draw_eight
+    beq $a2, 9, draw_nine      # If digit is 9, call draw_nine
+    # jr $ra
+
+draw_zero: 
     jal draw_horizontal
     addi $t0, $t0, 1792 #BOTTOM HORIZONTAL
     jal draw_horizontal 
-    
     addi $t0, $t0, -1792 #LEFT TOP VERT
     jal draw_vertical
     addi $t0, $t0, 12 #RIGHT TOP VERT
@@ -20,8 +66,8 @@ draw_zero:
     jr $ra
 
 draw_one: 
-    lw $t0, ADDR_DSPL
-    addi $t0, $t0, 1452 #TOP HORIZONTAL
+    # lw $t0, ADDR_DSPL
+    # addi $t0, $t0, 1452 #TOP HORIZONTAL
     addi $t0, $t0, 12 #RIGHT TOP VERT
     jal draw_vertical
     addi $t0, $t0, 1024 #RIGHT BOTTOM VERT
@@ -29,8 +75,8 @@ draw_one:
     jr $ra
 
 draw_two: 
-    lw $t0, ADDR_DSPL 
-    addi $t0, $t0, 1452 #TOP HORIZONTAL
+    # lw $t0, ADDR_DSPL 
+    # addi $t0, $t0, 1452 #TOP HORIZONTAL
     jal draw_horizontal
     addi $t0, $t0, 768 #MID HORIZONTAL
     jal draw_horizontal 
@@ -45,11 +91,11 @@ draw_two:
     jr $ra
 
 draw_three: 
-    lw $t0, ADDR_DSPL
+    # lw $t0, ADDR_DSPL
     # TO BE REMOVED; initialize $t0 at summoning point
     #Each respective digit of ABCD takes an offset of 0, 20, 40, 60 - an exercise to the implementer. 
     # addi $t0, $t0, 60
-    addi $t0, $t0, 1452 #TOP HORIZONTAL
+    # addi $t0, $t0, 1452 #TOP HORIZONTAL
     jal draw_horizontal
     
     addi $t0, $t0, 768 #MID HORIZONTAL
@@ -65,8 +111,8 @@ draw_three:
     jr $ra
 
 draw_four: 
-    lw $t0, ADDR_DSPL 
-     addi $t0, $t0, 1452 #TOP HORIZONTAL
+    # lw $t0, ADDR_DSPL 
+     # addi $t0, $t0, 1452 #TOP HORIZONTAL
     addi $t0, $t0, 768 #MID HORIZONTAL
     jal draw_horizontal 
     addi $t0, $t0, 256 #MID HORIZONTAL
@@ -80,11 +126,11 @@ draw_four:
     jr $ra
 
 draw_five: 
-    lw $t0, ADDR_DSPL
+    # lw $t0, ADDR_DSPL
     # TO BE REMOVED; initialize $t0 at summoning point
     #Each respective digit of ABCD takes an offset of 0, 20, 40, 60 - an exercise to the implementer. 
     # addi $t0, $t0, 60
-    addi $t0, $t0, 1452 #TOP HORIZONTAL
+    # addi $t0, $t0, 1452 #TOP HORIZONTAL
     jal draw_horizontal
     
     addi $t0, $t0, 768 #MID HORIZONTAL
@@ -102,11 +148,11 @@ draw_five:
     jr $ra
 
 draw_six: 
-    lw $t0, ADDR_DSPL
+    # lw $t0, ADDR_DSPL
     # TO BE REMOVED; initialize $t0 at summoning point
     #Each respective digit of ABCD takes an offset of 0, 20, 40, 60 - an exercise to the implementer. 
     # addi $t0, $t0, 60
-    addi $t0, $t0, 1452 #TOP HORIZONTAL
+    # addi $t0, $t0, 1452 #TOP HORIZONTAL
     jal draw_horizontal
     
     addi $t0, $t0, 768 #MID HORIZONTAL
@@ -126,11 +172,11 @@ draw_six:
     jr $ra
 
 draw_seven: 
-    lw $t0, ADDR_DSPL
+    # lw $t0, ADDR_DSPL
     # TO BE REMOVED; initialize $t0 at summoning point
     #Each respective digit of ABCD takes an offset of 0, 20, 40, 60 - an exercise to the implementer. 
     # addi $t0, $t0, 60
-    addi $t0, $t0, 1452 #TOP HORIZONTAL
+    # addi $t0, $t0, 1452 #TOP HORIZONTAL
     jal draw_horizontal
     addi $t0, $t0, 12 #RIGHT TOP VERT
     jal draw_vertical
@@ -139,11 +185,11 @@ draw_seven:
     jr $ra
 
 draw_eight: 
-    lw $t0, ADDR_DSPL
+    # lw $t0, ADDR_DSPL
     # TO BE REMOVED; initialize $t0 at summoning point
     #Each respective digit of ABCD takes an offset of 0, 20, 40, 60 - an exercise to the implementer. 
     # addi $t0, $t0, 60
-    addi $t0, $t0, 1452 #TOP HORIZONTAL
+    # addi $t0, $t0, 1452 #TOP HORIZONTAL
     jal draw_horizontal
     
     addi $t0, $t0, 768 #MID HORIZONTAL
@@ -165,11 +211,11 @@ draw_eight:
     jr $ra
     
 draw_nine: 
-    lw $t0, ADDR_DSPL
+    # lw $t0, ADDR_DSPL
     # TO BE REMOVED; initialize $t0 at summoning point
     #Each respective digit of ABCD takes an offset of 0, 20, 40, 60 - an exercise to the implementer. 
     # addi $t0, $t0, 60
-    addi $t0, $t0, 1452 #TOP HORIZONTAL
+    # addi $t0, $t0, 1452 #TOP HORIZONTAL
     jal draw_horizontal
     
     addi $t0, $t0, 768 #MID HORIZONTAL
